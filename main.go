cf7879eeec8b6edea04f3b933ff711864425e085
Browny <julian.kramer.berlin@icloud.com>
2022-08-28 14:41:44 +0200

    add documentation

--- main.go
+++ main.go
@@ -8,15 +8,16 @@ import (
 )
 
 type ImageData struct {
-	BgImgPath string
-	FontPath  string
-	FontSize  float64
-	Text      string
-	X         float64
-	Y         float64
-	Color     color.Color
+	BgImgPath string      // path to the image to add the text on
+	FontPath  string      // path to a .ttf file containgin the font of the text (always needed)
+	FontSize  float64     // size of the text i.e. 20
+	Text      string      //
+	X         float64     // the X coordinate of the text
+	Y         float64     // the Y coorginate of the text
+	Color     color.Color // font color
 }
 
+// main function to add a text to an Image from an given ImageData structure
 func AddTextToImage(imageData ImageData) (image.Image, error) {
 	bgImage, err := gg.LoadImage(imageData.BgImgPath)
 	if err != nil {
@@ -40,6 +41,7 @@ func AddTextToImage(imageData ImageData) (image.Image, error) {
 	return dc.Image(), nil
 }
 
+// save the given Image at the given path as png
 func Save(img image.Image, path string) error {
 	if err := gg.SavePNG(path, img); err != nil {
 		return err
